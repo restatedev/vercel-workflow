7f43916f7f1bb8da2c3a5e0d54cfb0f56f8521e7
Nik Nasr <nik@restate.dev>
2025-10-24 09:50:52 +0100

    add examples
    Signed-off-by: Nik Nasr <nik@restate.dev>

---
+++ packages/examples/workflow/src/app/api/signup/route.ts
@@ -0,0 +1,14 @@
+import { start } from "workflow/api";
+import { handleUserSignup } from "../../../workflows/user-signup.js";
+import { NextResponse } from "next/server.js";
+
+export async function POST(request: Request) {
+  const { email } = (await request.json()) as { email: string };
+
+  // Executes asynchronously and doesn't block your app
+  await start(handleUserSignup, [email]);
+
+  return NextResponse.json({
+    message: "User signup workflow started",
+  });
+}
